46998f29f6aab7a29cf604d2a2665156ce824b82
irfan.manullang <ifmanullang@gmail.com>
2023-01-11 14:58:30 +0700

    posweb

---
+++ POS/POS.DataContext/CategoryEntity.cs
@@ -0,0 +1,24 @@
+﻿using POS.Repository;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace POS.DataContext
+{
+        [Table("tbl_category")]
+        public class CategoryEntity
+    {
+        [Key]
+
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("category_name")]
+        public String CategoryName { get; set; }
+
+        [Column("description")]
+        public String Description { get; set; }
+
+        public ICollection<ProductEntity> productEntities { get; set; }
+
+    }
+}
---
+++ POS/POS.Web/DataContext/AppContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Repository
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+
+        }
+        public DbSet<CategoryEntity> categoryEntities => Set<CategoryEntity>();
+        public DbSet<ProductEntity> productEntities => Set<ProductEntity>();
+        public DbSet<SupplierEntity> supplierEntities => Set<SupplierEntity>();
+        public DbSet<OrderEntity> ordersEntities => Set<OrderEntity>();
+        public DbSet<CustomersEntity> customersEntities => Set<CustomersEntity>();
+        public DbSet<OrderDetailEntity> orderDetailEntities => Set<OrderDetailsEntity>();
+        public DbSet<EmployeEntity> employeEntities => Set<EmployeEntity>();
+
+    }
+}
